434a9e1a2982cde1c45a478b13691069aa1e8888
Heidi Sprouse <hsprouse91@gmail.com>
2020-06-08 15:12:29 -0500

    initial commit

---
+++ Roommates/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Roommates.Models;
+using Roommates.Repositories;
+
+namespace Roommates
+{
+    class Program
+    {
+        /// <summary>
+        ///  This is the address of the database.
+        ///  We define it here as a constant since it will never change.
+        /// </summary>
+        private const string CONNECTION_STRING = @"server=localhost\SQLExpress;database=Roommates;integrated security=true";
+
+        static void Main(string[] args)
+        {
+            RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
+            RoommateRepository roommateRepo = new RoommateRepository(CONNECTION_STRING);
+
+            Console.WriteLine("Getting All Rooms:");
+            Console.WriteLine();
+
+            List<Room> allRooms = roomRepo.GetAll();
+
+            foreach (Room room in allRooms)
+            {
+                Console.WriteLine($"{room.Id} {room.Name} {room.MaxOccupancy}");
+            }
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Getting All Roommates:");
+            Console.WriteLine();
+
+            List<Roommate> allRoommates = roommateRepo.GetAll();
+
+            foreach (Roommate roommate in allRoommates)
+            {
+                Console.WriteLine($"{roommate.Id} {roommate.Firstname} {roommate.Lastname} {roommate.RentPortion} {roommate.MovedInDate}");
+            }
+
+            Console.WriteLine("Get roommate by Id: ");
+            Roommate aRoommate = roommateRepo.GetById(2);
+            Console.WriteLine($"{aRoommate.Id} {aRoommate.Firstname} {aRoommate.Lastname} {aRoommate.RentPortion} {aRoommate.MovedInDate}");
+
+
+        }
+    }
+}
---
+++ Roommates/Repositories/RoommateRepository.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Roommates.Repositories
+{
+    //make a new repository class that inherits from the base repository
+    public class RoommateRepository : BaseRepository
+    {
+        /// <summary>
+        ///  When new RoommateRespository is instantiated, pass the connection string along to the BaseRepository
+        /// </summary>
+        public RoommateRepository(string connectionString) : base(connectionString) { }
+
+        public List<Roommate> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, FirstName, LastName, RentPortion, MoveInDate, RoomId FROM Roommate";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Roommate> roommates = new List<Roommate>();
+
+                    while (reader.Read())
+                    {
+                        int idColumnPosition = reader.GetOrdinal("Id");
+
+                        int idValue = reader.GetInt32(idColumnPosition);
+
+                        int firstNameColumnPosition = reader.GetOrdinal("FirstName");
+                        string firstNameValue = reader.GetString(firstNameColumnPosition);
+
+                        int lastNameColumnPosition = reader.GetOrdinal("LastName");
+                        string lastNameValue = reader.GetString(lastNameColumnPosition);
+
+                        int rentPortionColumnPosition = reader.GetOrdinal("RentPortion");
+                        int rentPortionValue = reader.GetInt32(rentPortionColumnPosition);
+
+                        int moveInDateColumnPosition = reader.GetOrdinal("MoveInDate");
+                        DateTime moveInDateValue = reader.GetDateTime(moveInDateColumnPosition);
+
+                        int roomIdColumnPosition = reader.GetOrdinal("RoomId");
+                        int roomIdValue = reader.GetInt32(roomIdColumnPosition);
+
+                        Roommate roommate = new Roommate
+                        {
+                            Id = idValue,
+                            Firstname = firstNameValue,
+                            Lastname = lastNameValue,
+                            RentPortion = rentPortionValue,
+                            MovedInDate = moveInDateValue,
+                            Room = null
+                        };
+
+                        roommates.Add(roommate);
+                    }
+
+                    reader.Close();
+                    return roommates;
+                }
+            }
+            
+        }
+
+        public Roommate GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT FirstName, LastName, RentPortion, MoveInDate, RoomId FROM Roommate WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Roommate roommate = null;
+
+                    // If we only expect a single row back from the database, we don't need a while loop.
+                    if (reader.Read())
+                    {
+                        roommate = new Roommate
+                        {
+                            Id = id,
+                            Firstname = reader.GetString(reader.GetOrdinal("FirstName")),
+                            Lastname = reader.GetString(reader.GetOrdinal("LastName")),
+                            RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion")),
+                            MovedInDate = reader.GetDateTime(reader.GetOrdinal("MoveInDate")),
+                            Room = null
+                        };
+                    }
+
+                    reader.Close();
+
+                    return roommate;
+                }
+            }
+        }
+
+        public List<Roommate> GetAllWithRoom()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, FirstName, LastName, RentPortion, MoveInDate, RoomId FROM Roommate JOIN Room ON Room.Id = Roommate.RoomId";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Roommate> roommates = new List<Roommate>();
+
+                    while (reader.Read())
+                    {
+                        int idColumnPosition = reader.GetOrdinal("Id");
+
+                        int idValue = reader.GetInt32(idColumnPosition);
+
+                        int firstNameColumnPosition = reader.GetOrdinal("FirstName");
+                        string firstNameValue = reader.GetString(firstNameColumnPosition);
+
+                        int lastNameColumnPosition = reader.GetOrdinal("LastName");
+                        string lastNameValue = reader.GetString(lastNameColumnPosition);
+
+                        int rentPortionColumnPosition = reader.GetOrdinal("RentPortion");
+                        int rentPortionValue = reader.GetInt32(rentPortionColumnPosition);
+
+                        int moveInDateColumnPosition = reader.GetOrdinal("MoveInDate");
+                        DateTime moveInDateValue = reader.GetDateTime(moveInDateColumnPosition);
+
+                        int roomIdColumnPosition = reader.GetOrdinal("RoomId");
+                        int roomIdValue = reader.GetInt32(roomIdColumnPosition);
+
+                        Room roommateRoom = new Room()
+                        {
+                            Id = roomIdValue,
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            MaxOccupancy = reader.GetInt32(reader.GetOrdinal("MaxOccupancy")),
+                        };
+
+                        Roommate roommate = new Roommate
+                        {
+                            Id = idValue,
+                            Firstname = firstNameValue,
+                            Lastname = lastNameValue,
+                            RentPortion = rentPortionValue,
+                            MovedInDate = moveInDateValue,
+                            Room = roommateRoom
+                        };
+
+                        roommates.Add(roommate);
+                    }
+
+                    reader.Close();
+
+                    return roommates;
+                }
+
+            }
+
+        }
+    }
+}
